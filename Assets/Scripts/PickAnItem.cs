68e2b2c0506f19e0b8f2da613c4c6b34b82c6b3d
Gözde <gkahraman03@gmail.com>
2023-12-26 21:58:20 +0300

    method for pick up method: rn only possible to pick and equip flashlight. Player can't pick other objects. WHERE IS THE PROBLEM?

---
+++ Assets/Scripts/PickAnItem.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PickAnItem : MonoBehaviour
+{
+    public GameObject flashLightOnPlayer; // add the real flashlight as the child of the player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashLightOnPlayer.SetActive(false);
+    }
+
+    public bool IsFlashLight(GameObject gameObject)
+    {
+        bool isFlashLight = gameObject.tag == "Flashlight";
+        return isFlashLight;
+    }
+
+    private void OnTriggerStay(Collider other) //pickable Item's collider
+    {
+        if (other.gameObject.tag == "Player")
+            //is the *other* gambeObject's tag Player
+        {
+            PickableItem p = gameObject.GetComponent<PickableItem>(); 
+            if (p != null)
+            {
+                
+                if (Input.GetKey(KeyCode.E))
+                {
+                    if (IsFlashLight(gameObject))
+                    {
+                        PickUpItem(gameObject); 
+                        flashLightOnPlayer.SetActive(true);
+                        //addInventory(gameObject);
+                        // add inventory method
+                    }
+                    else
+                    {
+                       PickUpItem(gameObject);
+                       print("Item is picked");
+                       // add inventory method 
+                    }
+                }
+            }
+           /* else
+            {
+                OnTriggerExit();
+            } */
+        }
+    }
+
+    void PickUpItem(GameObject g)
+    {
+        g.gameObject.SetActive(false);
+        Debug.Log("Item is picked up");
+        return;
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
